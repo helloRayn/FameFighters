ba32ff6eda905f90da3f90b898624ca3fe4fccdb
helloRayn <helloryanmurphy@gmail.com>
2016-07-19 00:16:02 +0100

    End of day 1

---
+++ Assets/Code/Player_Move.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Move : MonoBehaviour
+{
+
+    Vector2 target = Vector2.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.transform.position = Vector3.MoveTowards(this.transform.position, target, 0.1f);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x = position.x - 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x = position.x + 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y = position.y + 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y = position.y - 0.1f;
+            this.transform.position = position;
+        }
+    }
+    public void New_target (Vector2 nTarget) {
+
+        target = nTarget;
+    }
+}
+
---
+++ Assets/Code/Town_Code.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Town_Code : MonoBehaviour {
+
+    public Player_Move Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray)) onclick();
+        }*/
+	}
+    void onclick () {
+
+        Debug.Log("target.x.ToString");
+    }
+    void OnMouseDown(){
+
+        Player.New_target(this.transform.position);
+        onclick();
+
+    }
+}
